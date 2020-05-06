761bfd5d55b6c6e68274d384905eca4a90378ea5
athabasca <athabasca@users.noreply.github.com>
2020-05-06 11:50:17 -0700

    reformat JS again

--- index.js
+++ index.js
@@ -3,71 +3,69 @@ const github = require('@actions/github');
 /*
 // When a new issue is opened, automatically add it to a GitHub project to facilitate review.
 function issue2project(octokit) {
-	// Trigger: issue opened
-	// issue context
-    // https://help.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions
-    // github.context.event is the webhook payload, in this case the issues event payload
-    // https://developer.github.com/v3/activity/events/types/#issuesevent
-    const issue = github.context.event.issue;
-	const owner = issue.repository.owner.login;
-	const repo = issue.repository.name;
-	
-	// Cribbing from github-actions-automate-projects
-	// https://github.com/takanabe/github-actions-automate-projects/blob/master/main.go
-	
-	// Find project ID
-	// https://octokit.github.io/rest.js/v17#projects-list-for-repo
-	// https://developer.github.com/v3/projects/
-	// https://octokit.github.io/rest.js/v17#pagination
-	const projectURL = core.getInput('projectURL');
-	var projectId = 0;
-	for await (const response of octokit.paginate.iterator(
-	  octokit.projects.listForRepo,
-	  {
-		owner: owner,
-		repo: repo,
-	  }
-	)) {
-		const { data : project } = response;
-		if (project.html_url == projectURL) { // brittle?
-				projectID = project.id;
-				break;
-		}
-	}
-	if (projectID == 0) { core.debug("No such project"); return; }
-	
-	// Find column ID
-	// https://octokit.github.io/rest.js/v17#projects-list-columns
-	// https://developer.github.com/v3/projects/columns/#list-project-columns
-	const colName = core.getInput('columnName');
-	var colId = 0;
-	for await (const response of octokit.paginate.iterator(
-	  octokit.projects.listColumns,
-	  {
-		project_id: projectID,
-	  }
-	)) {
-		const { data : column } = response;
-		if (column.name == colName) { // brittle?
-				colId = column.id;
-				break;
-		}
-	}	
-	if (colID == 0) { core.debug("No such column"); return; }
-	
-	// Create project card from issue 
-	// https://developer.github.com/v3/projects/cards/#create-a-project-card
-	// https://octokit.github.io/rest.js/v17#projects-create-card
-	const { data: card } = octokit.projects.createCard({
-		column_id: colId,
-		content_id: github.context.event.issue.id,
-		content_type: 'Issue',
-	});
-	
-	// Store resulting card ID (will be needed for `New PR to Project Column`)
-	const cardId = card.id;
+// Trigger: issue opened
+// issue context
+// https://help.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions
+// github.context.event is the webhook payload, in this case the issues event payload
+// https://developer.github.com/v3/activity/events/types/#issuesevent
+const issue = github.context.event.issue;
+const owner = issue.repository.owner.login;
+const repo = issue.repository.name;
+
+// Cribbing from github-actions-automate-projects
+// https://github.com/takanabe/github-actions-automate-projects/blob/master/main.go
+
+// Find project ID
+// https://octokit.github.io/rest.js/v17#projects-list-for-repo
+// https://developer.github.com/v3/projects/
+// https://octokit.github.io/rest.js/v17#pagination
+const projectURL = core.getInput('projectURL');
+var projectId = 0;
+for await (const response of octokit.paginate.iterator(
+octokit.projects.listForRepo,{
+owner: owner,
+repo: repo,
 }
-*/
+)) {
+const { data : project } = response;
+if (project.html_url == projectURL) { // brittle?
+projectID = project.id;
+break;
+}
+}
+if (projectID == 0) { core.debug("No such project"); return; }
+
+// Find column ID
+// https://octokit.github.io/rest.js/v17#projects-list-columns
+// https://developer.github.com/v3/projects/columns/#list-project-columns
+const colName = core.getInput('columnName');
+var colId = 0;
+for await (const response of octokit.paginate.iterator(
+octokit.projects.listColumns,{
+project_id: projectID,
+}
+)) {
+const { data : column } = response;
+if (column.name == colName) { // brittle?
+colId = column.id;
+break;
+}
+}
+if (colID == 0) { core.debug("No such column"); return; }
+
+// Create project card from issue
+// https://developer.github.com/v3/projects/cards/#create-a-project-card
+// https://octokit.github.io/rest.js/v17#projects-create-card
+const { data: card } = octokit.projects.createCard({
+column_id: colId,
+content_id: github.context.event.issue.id,
+content_type: 'Issue',
+});
+
+// Store resulting card ID (will be needed for `New PR to Project Column`)
+const cardId = card.id;
+}
+ */
 // When an issue is given the ‘review’ label, convert it to a pull request.
 function issue2pr(octokit) {
     // Trigger: issue is given 'review' label
@@ -78,8 +76,8 @@ function issue2pr(octokit) {
     const issue = github.context.event.issue;
     const owner = issue.repository.owner.login;
     const repo = issue.repository.name;
-	
-	core.debug("woo printf debugging");
+
+    core.debug("woo printf debugging");
 
     // get reference
     // https://developer.github.com/v3/git/refs/#get-a-single-reference
@@ -112,7 +110,7 @@ function issue2pr(octokit) {
             data
         }) => {
         // handle data
-		core.debug(data);
+        core.debug(data);
     });
 
     // create file from issue body and commit it to the branch
@@ -137,7 +135,7 @@ function issue2pr(octokit) {
             data
         }) => {
         // handle data
-		core.debug(data);
+        core.debug(data);
     });
 
     // create pull request for the branch
@@ -159,33 +157,33 @@ function issue2pr(octokit) {
             data
         }) => {
         // handle data
-		core.debug(data);
+        core.debug(data);
     });
 }
 
 // When an issue is converted to a pull request, move the associated card to next column.
 //function movePRcard(octokit) {
-	// Trigger: PullRequestEvent opened
-	// https://developer.github.com/v3/activity/events/types/#pullrequestevent
-	
-	// Get project card ID
-	// Download card ID with actions/download-artifact in workflow and input to this action?
-	
-	// Move a project card
-	// https://developer.github.com/v3/projects/cards/#move-a-project-card
-	// https://octokit.github.io/rest.js/v17#projects-move-card
+// Trigger: PullRequestEvent opened
+// https://developer.github.com/v3/activity/events/types/#pullrequestevent
+
+// Get project card ID
+// Download card ID with actions/download-artifact in workflow and input to this action?
+
+// Move a project card
+// https://developer.github.com/v3/projects/cards/#move-a-project-card
+// https://octokit.github.io/rest.js/v17#projects-move-card
 //}
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
         console.log("am I here?");
-		core.debug("where am I?");
-		// This should be a token with access to your repository scoped in as a secret.
+        core.debug("where am I?");
+        // This should be a token with access to your repository scoped in as a secret.
         // The YML workflow will need to set myToken with the GitHub Secret Token
         // myToken: ${{ secrets.GITHUB_TOKEN }}
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-       	   const myToken = core.getInput('myToken');
+        const myToken = core.getInput('myToken');
 
         // Authenticated Octokit client
         const octokit = new github.GitHub(myToken);
